0fc33bf7f2408f2bb684ae3f87814b8091ded59d
Tomislav Marinkovic <tomislav.marinkovic@providers.glovoapp.com>
2021-12-03 17:18:54 +0100

    add fetching of data mesh definition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+const App = () => {
+
+    const lambdaUrl = "https://9mebwsh049.execute-api.eu-west-2.amazonaws.com/dev/"
+
+    useEffect(() => {
+        fetch(lambdaUrl)
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
