6146bc0458dc8403f4a915964b409e6d65afa4c9
Tomislav Marinkovic <tomislav.marinkovic@providers.glovoapp.com>
2021-12-29 09:39:20 +0100

    add legend

--- src/components/NetworkView/NetworkView.js
+++ src/components/NetworkView/NetworkView.js
@@ -2,6 +2,7 @@ import Sigma from "react-sigma";
 import {useDispatch, useSelector} from "react-redux";
 import getNetworkSpecification from "./networkSpecification";
 import {setDataProduct, setDomain, setInputPort, setOutputPort} from "../../app/reducers/selected";
+import './NetworkView.css'
 
 const NetworkView = () => {
 
@@ -20,18 +21,33 @@ const NetworkView = () => {
     const drawNetwork = () => {
         if (graph.nodes.length !== 0 && graph.edges.length !== 0) {
             return (
-                <Sigma
-                    style={{height: "380px"}}
-                    onClickNode={e => onNodeClick(e)}
-                    graph={graph}
-                    settings={
-                        {
-                            drawEdges: true,
-                            clone: false,
-                            labelThreshold: 0
-                        }
-                    }>
-                </Sigma>
+                <div style={{position: "relative"}}>
+                    <div className="network-legend">
+                        <div className="circle input-port"/>
+                        Input Port
+                        <br/>
+                        <div style={{clear: "both"}}/>
+                        <div className="circle data-product"/>
+                        Data Product
+                        <br/>
+                        <div style={{clear: "both"}}/>
+                        <div className="circle output-port"/>
+                        Output port
+                        <br/>
+                    </div>
+                    <Sigma
+                        style={{height: "380px"}}
+                        onClickNode={e => onNodeClick(e)}
+                        graph={graph}
+                        settings={
+                            {
+                                drawEdges: true,
+                                clone: false,
+                                labelThreshold: 0
+                            }
+                        }>
+                    </Sigma>
+                </div>
             )
         }
     }
